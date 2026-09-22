14e917969d7e5b30985b515431b9de54426d5c5e
Satyam Saurabh <149925640+satyamsaurabh1@users.noreply.github.com>
2026-09-23 00:41:28 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,7 +4,7 @@ public:
         int low = 0, mid = 0, high = nums.size()-1;
         while(mid <= high){
             if(nums[mid] == 0){
-                swap(nums[low], nums[mid]);
+                swap(nums[mid],nums[low]);
                 low++;
                 mid++;
             }
